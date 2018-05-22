3db4705f4b3d7e10f9a3b7e8e2218ca5e8405f8e
IFM\JIoppolo <jioppolo@infomedia.com.au>
2018-05-22 12:26:17 +1000

    Added DataService with rxjs subject and observable and two public methods to trigger visbility and reset the app

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private visibleSubject = new Subject<boolean>();
+  visibleObservable: Observable<boolean> = this.visibleSubject.asObservable();
 
-  constructor() { }
+  triggerVisibility() {
+    this.visibleSubject.next(true);
+  }
+  
+  reset() {
+    this.visibleSubject.next(false);
+  }
 }
